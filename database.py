c0aa3e4c7333340df24f8fd5cc1cdcf081adfd61
a-ge <amgesmundo@gmail.com>
2021-07-23 16:32:08 -0700

    modified database, model, server files

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ def connect_to_db(app):
     """Connect the database to our Flask app."""
 
     # Configure to use our PstgreSQL database
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tracker"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///movieQuotes"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
--- model.py
+++ model.py
@@ -1,7 +1,4 @@
-"""Models and database functions for final project."""
-# from SQLAlchemy import timestamp
-import datetime
-
+"""Models and database functions."""
 # from database import *
 
 from flask_sqlalchemy import SQLAlchemy
@@ -10,137 +7,54 @@ db = SQLAlchemy()
 
 # Model definitions
 
+class Character(db.Model):
+    """"Characters"""
 
-class User(db.Model):
-    """"User of website"""
+    __tablename__ = "characters"
 
-    __tablename__ = "users"
-
-    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    first_name = db.Column(db.String(200), nullable=True)
-    last_name = db.Column(db.String(200), nullable=True)
-    email = db.Column(db.String(200))
-    password = db.Column(db.String(100))
+    character_id = db.Column(db.String(200), primary_key=True)
+    character_name = db.Column(db.String(200), nullable=True)
 
     # many to many relationship
-    moods = db.relationship("Mood", backref="users", secondary="entries")
+    characters = db.relationship("Mood", backref="users", secondary="entries")
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return f"<User user_id={self.user_id} email={self.email}>"
+        return f"<Character character_id={self.character_id} character_name={self.character_name}>"
 
 
-class Mood(db.Model):
-    """Moods users can select from"""
+class Movie(db.Model):
+    """Movies"""
 
-    __tablename__ = "moods"
+    __tablename__ = "movies"
 
-    mood_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    mood = db.Column(db.String(30))
-    verbose_mood = db.Column(db.String(30))
-
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-
-        return f"<Mood mood_id={self.mood_id} mood={self.mood} verbose_mood={self.verbose_mood}>"
-
-
-class Activity_Category(db.Model):
-    """"Activties users can select from"""
-
-    __tablename__ = "activities"
-
-    activity_category_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    category = db.Column(db.String(30))
-    verbose_category = db.Column(db.String(30))
+    movie_id = db.Column(db.String(200), primary_key=True)
+    movie_title = db.Column(db.String(30))
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return f"<Activity activity_category_id={self.activity_category_id} category={self.category}>"
+        return f"<Movie movie_id={self.movie_id} movie_title={self.movie_title}>"
 
 
-class Entry_Activity(db.Model):
+class Quote(db.Model):
+    """"Quotes"""
 
-    __tablename__ = "entry_activities"
+    __tablename__ = "quotes"
 
-    entry_activity_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    entry_id = db.Column(db.Integer, db.ForeignKey("entries.entry_id"))
-    activity_category_id = db.Column(
-        db.Integer, db.ForeignKey("activities.activity_category_id")
-    )
-
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-
-        return f"<Entry_Activity entry_id={self.entry_id} activity_category_id={self.activity_category_id}>"
-
-
-class Entry(db.Model):
-    """Daily entries for each user"""
-
-    __tablename__ = "entries"
-
-    entry_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
-    mood_id = db.Column(db.Integer, db.ForeignKey("moods.mood_id"))
-    description = db.Column(db.Text, nullable=True)
+    quote_id = db.Column(db.String(200), primary_key=True)
+    character_id = db.Column(db.String(200), db.ForeignKey("characters.character_id"))
+    movie_id = db.Column(db.String(200), db.ForeignKey("movies.movie_id"))
 
     # one to many relationship
-    user = db.relationship("User", backref="entries")
-    mood = db.relationship("Mood", backref="entries")
-
-    # many to many
-    activities = db.relationship(
-        "Activity_Category", backref="entries", secondary="entry_activities"
-    )
+    character = db.relationship("Character", backref="quotes")
+    movie = db.relationship("Movie", backref="quotes")
 
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return f"<Entry entry_id={self.entry_id} date_created={self.date_created} user_id={self.user_id} mood_id={self.mood_id} description={self.description}>"
-
-
-class Mood_Enhancer(db.Model):
-    """Optional inputed description from user to enhance mood"""
-
-    __tablename__ = "mood_enhancers"
-
-    mood_enhancer_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
-    mood_enhancer = db.Column(db.Text, nullable=True)
-
-    users = db.relationship("User", backref="mood_enhancers")
-
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-
-        return f"""<Mood Enhancer mood_enhancer_id={self.mood_enhancer_id}
-            user_id={self.user_id} mood_enhancer={self.mood_enhancer}>"""
-
-
-class User_Brain_Dump(db.Model):
-    """Optional inputed description from user as a diary entry """
-
-    __tablename__ = "user_brain_dumps"
-
-    user_brain_dump_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
-    brain_dump_entry = db.Column(db.Text, nullable=True)
-    analysis_confirmation = db.Column(db.String(10), nullable=True)
-    verbose_analysis = db.Column(db.String(10), nullable=True)
-    analysis = db.Column(db.DECIMAL(18, 4), nullable=True)
-
-    users = db.relationship("User", backref="user_brain_dumps")
-
-    def __repr__(self):
-        """Provide helpful representation when printed."""
-
-        return f"""<User Brain Dump user_brain_dump_id={self.user_brain_dump_id}
-            user_id={self.user_id}>"""
+        return f"<Quote quote_id={self.quote_id} character_id={self.character_id} movie_id={self.movie_id}>"
 
 
 ################################################################################
@@ -151,12 +65,12 @@ def connect_to_db(app):
     """Connect the database to our Flask app."""
 
     # Configure to use our PstgreSQL database
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tracker"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///movieQuotes"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     # Used to recreate my database if I need to drop
-    db.create_all()
+    # db.create_all()
 
 
 if __name__ == "__main__":
